312c15710f21db166e637c5331926c6fe9265546
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-03-25 15:33:06 -0700

    installed mongoose and react-router-dom, connected to mongodb

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mongoose = require('mongoose');
+// const routes = require("./routes");
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,9 +15,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-app.use(routes);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lauracullen");
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/');
+// app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
