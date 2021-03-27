cff8151c87ae361b91fbc2c6a01d9cd30ad83bad
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-03-27 11:59:16 -0700

    parallax layout of about page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Nav from "./components/Nav";
+// import Nav from "./components/Nav";
+import About from "./components/About";
 
 function App() {
 return (
   <Router>
     <div>
-      <Nav />
+      {/* <Nav /> */}
+      <About />
     </div>
   </Router>
 )
