44ee86c901385c1e7a43870d0ca959d35899b017
Lili Cohen <lili.cohen.tv@gmail.com>
2021-03-27 13:04:41 -0700

    fixing issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Nav from "./components/Nav";
-import About from "./components/About";
-=======
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import About from './components/About';
 import Nav from './components/Nav';
 import LoginModal from './components/LoginModal';
 // import ModalButton from './components/ModalButton';
->>>>>>> main
 
 function App() {
   const [show, setShow] = useState(false);
@@ -27,18 +21,14 @@ function App() {
 
   return (
     <div>
-<<<<<<< HEAD
-      {/* <Nav /> */}
-      <About />
-=======
       <Router>
         <div>
           <Nav />
+          <About />
         </div>
       </Router>
       <button onClick={() => setShow(true)}>Open Modal</button>
       <LoginModal onClose={() => setShow(false)} show={show} />
->>>>>>> main
     </div>
   );
 }
