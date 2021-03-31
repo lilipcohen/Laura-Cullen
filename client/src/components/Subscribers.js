49791cc119fff15b472f0e52ef1da7a93af6345e
Lili Cohen <lili.cohen.tv@gmail.com>
2021-03-30 21:33:58 -0700

    adding routing to login and logout and adding subscriber route to test with

---
+++ client/src/components/Subscribers.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Subscribers = () => {
+  return (
+    <div>
+      <h1>Newsletter</h1>
+      <ul>
+        <li>email.yahoo.com</li>
+        <li>email.yahoo.com</li>
+        <li>email.yahoo.com</li>
+        <li>email.yahoo.com</li>
+        <li>email.yahoo.com</li>
+        <li>email.yahoo.com</li>
+        <li>email.yahoo.com</li>
+        <li>email.yahoo.com</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Subscribers;
